c6fb5bfd9d75de5506402de98b3ed65410035efa
Syrymbek Zhumazhan <75174477+srmbackisdeveloper@users.noreply.github.com>
2024-10-13 04:07:33 +0500

    feat (gateway) : http server

--- gateway/cmd/api/main.go
+++ gateway/cmd/api/main.go
@@ -10,15 +10,15 @@ import (
 
 func main() {
 	grpcAddress := os.Getenv("USERS_GRPC_ADDRESS")
-	log.Printf("Connecting to users service at: %s", grpcAddress)
+	log.Printf("Connecting to U service at: %s", grpcAddress)
 	
 	srv, err := server.NewServer(grpcAddress)
     if err != nil {
-        log.Fatalf("GATEWAY: Failed to create server: %s", err)
+        log.Fatalf("GATEWAY service: Failed to create server: %s", err)
     }
 
 	err = srv.ListenAndServe()
     if err != nil {
-        log.Fatalf("GATEWAY: Failed to start server: %s", err)
+        log.Fatalf("GATEWAY service: Failed to start server: %s", err)
     }
 }
